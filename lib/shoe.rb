eb28b751fe79ec283a5373c283b10a33882778c5
Hannah Harrington <hannahharrington@outlook.com>
2019-11-09 19:19:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
   def initialize(brand)
     @brand = brand
-end
+
 end
 def cobble.new
     puts "cobble says that the shoe has been repaired"
